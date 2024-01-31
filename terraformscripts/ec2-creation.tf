a1e039cbdfc4ee07a1965be59e60d2a86ed3a8b1
digitalm2016 <98210819+digitalm2016@users.noreply.github.com>
2024-01-31 15:51:25 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,16 +1,16 @@
-provider "aws" {
+provider "aws"{
 region = "ap-south-1"
 }
-resource "aws_instance" "AWSEC2Instance"{
-  ami = "ami-00952f27cf14db9cd"
-  instance_type = "t2.micro"
-  key_name = "devpemkey"
-  security_groups = ["launch-wizard-13"]
-  tags{
-    Name = "tomcat_server_new"
-  }
-}
 
+resource "aws_instance" "AWSEC2Instance"{
+ami = "ami-00952f27cf14db9cd"
+instance_type = "t2.micro"
+key_name = "devpemkey"
+security_groups = ["launch-wizard-2"]
+tags={
+Name = "tomcatserversnew"
+}
+}
 
 
 
