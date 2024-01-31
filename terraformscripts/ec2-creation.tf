4aae201157dfc5e8848823731b7980cb52982e7d
digitalm2016 <98210819+digitalm2016@users.noreply.github.com>
2024-01-31 15:37:12 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-region="ap-south-1"
+region = "ap-south-1"
 }
-resource "aws_instance" "AWSEC2INSTANCE" {
+resource "aws_instance" "AWSEC2INSTANCE"{
   ami="ami-00952f27cf14db9cd"
   instance_type="t2.micro"
   key_name="devpemkey"
